ff1dfdef8e8362a339e72dbcced5dd8f673b6f45
sky121016 <sky121016@naver.com>
2025-06-16 14:11:22 +0900

    build 수정

--- src/main/java/com/welab/k8s_api_gateway/config/WebSecurityConfig.java
+++ src/main/java/com/welab/k8s_api_gateway/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.welab.k8s_api_gateway.config;
 
+import com.welab.k8s_api_gateway.security.exception.RestAccessDeniedHandler;
+import com.welab.k8s_api_gateway.security.exception.RestAuthenticationEntryPoint;
 import com.welab.k8s_api_gateway.security.filter.JwtAuthenticationFilter;
 import com.welab.k8s_api_gateway.security.jwt.JwtTokenValidator;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final JwtTokenValidator jwtTokenValidator;
+    private final RestAuthenticationEntryPoint authenticationEntryPoint;
+    private final RestAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
@@ -37,6 +41,10 @@ public class WebSecurityConfig {
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtTokenValidator),
                         UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((exceptionConfig) ->
+                        exceptionConfig
+                                .authenticationEntryPoint(authenticationEntryPoint)
+                                .accessDeniedHandler(accessDeniedHandler))
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/api/user/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
@@ -50,7 +58,8 @@ public class WebSecurityConfig {
         config.setAllowCredentials(true);
 // config.setAllowedOrigins(List.of("*"));
         config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
--- src/main/java/com/welab/k8s_api_gateway/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/welab/k8s_api_gateway/security/filter/JwtAuthenticationFilter.java
@@ -2,12 +2,16 @@ package com.welab.k8s_api_gateway.security.filter;
 
 import com.welab.k8s_api_gateway.security.jwt.JwtTokenValidator;
 import com.welab.k8s_api_gateway.security.jwt.authentication.JwtAuthentication;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
